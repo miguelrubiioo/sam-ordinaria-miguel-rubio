0784cebc1d263d255fa9e875c95ee678ba5e3733
miguelrubiioo <146731914+miguelrubiioo@users.noreply.github.com>
2024-03-13 13:44:43 +0100

    Update put-item.mjs

--- src/handlers/put-item.mjs
+++ src/handlers/put-item.mjs
@@ -21,14 +21,19 @@ export const putItemHandler = async (event) => {
 
     // Get id and name from the body of the request
     const body = JSON.parse(event.body);
+    const body = JSON.parse(event.body);
     const id = body.id;
-    const name = body.name;
+    const petName= body.petName;
+    const animalSpecies = body.animalSpecies;
+    const age = body.age;
+    const lastHealthCheck = body.lastHealthCheck;
 
     // Creates a new item, or replaces an old item with a new item
     // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
     var params = {
         TableName : tableName,
-        Item: { id : id, name: name }
+        Item: { id : id, petName: petName, animalSpecies: animalSpecies, age: age,
+        lastHealthCheck: lastHealthCheck}
     };
 
     try {
